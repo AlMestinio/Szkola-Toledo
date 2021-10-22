9815e4722a9bb22275d4ed22838c115e320276e5
arturostrowski <artur.ostrowski.dev@gmail.com>
2021-10-23 00:46:42 +0200

    [ST-8] - Navigation component. Signout from firebase.

--- app/src/main/java/pl/ostrowskiartur/szkolatoledo/MainActivity.kt
+++ app/src/main/java/pl/ostrowskiartur/szkolatoledo/MainActivity.kt
@@ -1,25 +1,28 @@
 package pl.ostrowskiartur.szkolatoledo
 
 import android.os.Bundle
-import android.util.Log
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
 import android.view.View
 import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
-import androidx.navigation.Navigation
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main_content.*
 import kotlinx.android.synthetic.main.content_main.*
 import pl.ostrowskiartur.szkolatoledo.utis.then
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     companion object {
         private const val LOGIN = "Login"
+        private const val REGISTER = "Register"
     }
 
     private lateinit var appBarConfiguration: AppBarConfiguration
@@ -37,16 +40,29 @@ class MainActivity : AppCompatActivity() {
 
         updateToolbar()
 
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_menu)
         supportActionBar?.setDisplayShowTitleEnabled(false)
 
         FirebaseApp.initializeApp(this)
 
+        initNavigationView()
+
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                drawer_layout.openDrawer(GravityCompat.START)
+                return true
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun initNavigationView() {
+        tvLogout?.setOnClickListener(this)
     }
 
     override fun onBackPressed() {
@@ -65,16 +81,29 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun updateToolbar() {
-        if (navController.currentDestination?.label != null && navController.currentDestination!!.label == LOGIN) {
+        if (navController.currentDestination?.label != null && (navController.currentDestination!!.label == LOGIN || navController.currentDestination!!.label == REGISTER)) {
             changeToolbarVisibility(false)
+            drawer_layout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
         } else {
             changeToolbarVisibility(true)
             tvTitle?.text = navController.currentDestination?.label
+            drawer_layout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
         }
     }
 
     private fun changeToolbarVisibility(isVisible: Boolean) {
         clToolbar?.visibility = (isVisible then View.VISIBLE ?: View.INVISIBLE)
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            tvLogout?.id -> {
+                FirebaseAuth.getInstance().signOut()
+                navController.navigateUp()
+                navController.navigate(R.id.loginFragment)
+                drawer_layout.closeDrawer(GravityCompat.START)
+            }
+        }
     }
 
 }
